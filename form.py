789c50835f058d5953849f5eaa0dd812cd8f9ae1
buyayub <micourse@protonmail.com>
2022-03-13 20:25:22 -0400

    reorganization part 1

--- form.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import Form, RecaptchaField
-from wtforms import TextField, PasswordField, TextAreaField, StringField, validators
-
-class RegisterForm(Form):
-    username = StringField('username', [validators.DataRequired(), validators.Length(max=255)])
-    password = PasswordField('password', [validators.DataRequired(),validators.Length(min=8)])
-    email = StringField('email', [validators.DataRequired(), validators.Length(min=6, max=35)])
-    recaptcha = RecaptchaField()
